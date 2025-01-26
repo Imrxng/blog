6bd80df6b7ba251000e4f14d620b8f332e056d20
imrxng <imgha@hotmail.com>
2025-01-26 23:18:21 +0100

    fix error deployment

--- types.ts
+++ types.ts
@@ -1,10 +1,8 @@
-import { ObjectId } from "mongodb";
-
 export interface Blog {
     title: string;
     date: string;
     description: string;
     image?: string;
     category: string[];
-    _id?: ObjectId;
+    _id?: string;
 }
