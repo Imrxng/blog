6b10a5acd86ace5e855beb097035a25921b0d1bc
imrxng <imgha@hotmail.com>
2025-02-09 14:21:33 +0100

    m

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
         <header id="#top">
             <p id="bigName">Imran <span>Ghaddoura</span></p>
             <Nav />
-            <img src="./images/duim.webp" alt="" id="headerDuimFoto" />
+            <img src="../images/duim.webp" alt="" id="headerDuimFoto" />
         </header>
     );
 };
