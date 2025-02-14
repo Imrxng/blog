9f8f7d3ad6fe83f4a4f3ef9c29a902ba48478727
imrxng <imgha@hotmail.com>
2025-02-14 17:32:13 +0100

    m

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
     return (
         <nav>
             <a href="/" className='linkNav'>Home</a>
-            <a href="https://imrxng.github.io/portfolio/" className='linkNav'>Over me</a>
+            <a href="https://imrxng.github.io/portfolio/" target="_blank" className='linkNav'>Over me</a>
             <a href="/blogs" className='linkNav'>Blogs</a>
         </nav>
     );
